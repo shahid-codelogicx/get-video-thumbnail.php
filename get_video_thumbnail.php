60c7b1048059b417ab1a90eb644160f96ea9da71
Shahid <59919466+shahid-codelogicx@users.noreply.github.com>
2020-04-18 02:06:50 +0530

    Fixed for any type of url (youtube or vimeo)

--- get_video_thumbnail.php
+++ get_video_thumbnail.php
@@ -10,34 +10,50 @@ function get_video_thumbnail( $src ) {
 
 	$url_pieces = explode('/', $src);
 	
-	if ( $url_pieces[2] == 'player.vimeo.com' ) { // If Vimeo
+	if ( $url_pieces[2] == 'player.vimeo.com' || $url_pieces[2] == 'vimeo.com') { // If Vimeo
         
-		  $id = $url_pieces[4];
-		  $hash = unserialize(file_get_contents('https://vimeo.com/api/v2/video/' . $id . '.php'));
+	    preg_match('%^https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|video\/|)(\d+)(?:$|\/|\?)(?:[?]?.*)$%im', $src, $match);
+	    $vimeo_id = $match[3];
+            $hash = unserialize(file_get_contents('https://vimeo.com/api/v2/video/' . $vimeo_id . '.php'));
     
-    /**
-     * Image Quality. 
-     * @High quality thumbnail: [thumbnail_large]
-     * @Medium quality thumbnail: [thumbnail_medium]
-     * @Low quality thumbnail: [thumbnail_small]
-     */
+	    /**
+	     * Image Quality. 
+	     * @High quality thumbnail: [thumbnail_large]
+	     * @Medium quality thumbnail: [thumbnail_medium]
+	     * @Low quality thumbnail: [thumbnail_small]
+	     */
             
-		  $thumbnail = $hash[0]['thumbnail_large'];
+	    $thumbnail = $hash[0]['thumbnail_large'];
 
-	}
-  elseif ( $url_pieces[2] == 'www.youtube.com' ) { // If Youtube
-  
-      $extract_id = explode('=', $url_pieces[3]);
-      $id = $extract_id[1];
-    /**
-     * Image Quality.
-     * @Maximum quality thumbnail: maxresdefault.jpg
-     * @High quality thumbnail: hqdefault.jpg
-     * @Medium quality thumbnail: mqdefault.jpg
-     * @Low quality thumbnail: sddefault.jpg
-    */
+        }elseif ( $url_pieces[2] == 'www.youtube.com' ||  $url_pieces[2] == 'youtu.be') { // If Youtube
     
-		  $thumbnail = 'https://img.youtube.com/vi/' . $id . '/hqdefault.jpg';
+           // Here is a sample of the URLs this regex matches: (there can be more content after the given URL that will be ignored)
+
+	   // http://youtu.be/dQw4w9WgXcQ
+	   // http://www.youtube.com/embed/dQw4w9WgXcQ
+	   // http://www.youtube.com/watch?v=dQw4w9WgXcQ
+	   // http://www.youtube.com/?v=dQw4w9WgXcQ
+	   // http://www.youtube.com/v/dQw4w9WgXcQ
+	   // http://www.youtube.com/e/dQw4w9WgXcQ
+	   // http://www.youtube.com/user/username#p/u/11/dQw4w9WgXcQ
+	   // http://www.youtube.com/sandalsResorts#p/c/54B8C800269D7C1B/0/dQw4w9WgXcQ
+	   // http://www.youtube.com/watch?feature=player_embedded&v=dQw4w9WgXcQ
+	   // http://www.youtube.com/?feature=player_embedded&v=dQw4w9WgXcQ
+
+	   // It also works on the youtube-nocookie.com URL with the same above options.
+	   // It will also pull the ID from the URL in an embed code (both iframe and object tags)
+
+           preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $src, $match);
+           $youtube_id = $match[1];
+	   /**
+	     * Image Quality.
+	     * @Maximum quality thumbnail: maxresdefault.jpg
+	     * @High quality thumbnail: hqdefault.jpg
+	     * @Medium quality thumbnail: mqdefault.jpg
+	     * @Low quality thumbnail: sddefault.jpg
+	   */
+    
+	   $thumbnail = "https://img.youtube.com/vi/$youtube_id/hqdefault.jpg";
 
 	}
 
